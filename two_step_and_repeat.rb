9e2310341529f7d43913c58b8d10c3c0f64c6ff0
Kate1234567891 <katehalle617@icloud.com>
2018-07-10 20:03:32 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -10,7 +10,14 @@ def first_steps
 end
 
 def a_few_more_steps
-  
+  puts "Right foot back"
+ sleep(0.5)
+puts "Left foot back"
+sleep(0.5)
+puts "Right foot back"
+sleep(0.5)
+puts "Stop"
+sleep(1)
   puts "Left foot back"
   sleep (0.5)
   puts "Right foot back"
